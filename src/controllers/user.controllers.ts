6401088aa367065e27affdff7822b43f6196dddd
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-09-07 13:14:40 -0500

    feat: add new endpoint to update user

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -28,3 +28,21 @@ export const getUsers = async (req: Request, res: Response) => {
       return res.status(500).json({ message: error.message });
   }
 };
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOneBy({ id: parseInt(req.params.id) });
+
+    if (!user) return res.status(404).json({ message: "User does not exists" });
+
+    User.update({ id: parseInt(id) }, req.body);
+
+    return res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/user.controllers";
+import {
+  createUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controllers";
 
 const router = Router();
 
 router.post("/users", createUser);
+
 router.get("/users", getUsers);
+
+router.put("/users/:id", updateUser);
 
 export default router;
